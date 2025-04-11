0adfad4645511691f7ca5916f3335a8bb8106c4e
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-04-11 20:51:23 +0500

    adds check winner method

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    static ArrayList<Integer> playerPositions = new ArrayList<>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<>();
+
     public static void main(String[] args) {
 
         char[][] gameBoard = {
@@ -29,6 +32,8 @@ public class Main {
             placePiece(gameBoard, cpuPos, "computer");
 
             printGameBoard(gameBoard);
+
+            checkWinner();
         }
 
     }
@@ -44,11 +49,13 @@ public class Main {
 
     public static void placePiece(char[][] gameBoard, int pos, String user) {
 
-        char symbol = 'X';
+        char symbol = ' ';
         if (user.equals("player")) {
             symbol = 'X';
+            playerPositions.add(pos);
         } else if(user.equals("computer")) {
             symbol = 'O';
+            cpuPositions.add(pos);
         }
 
         switch(pos) {
@@ -82,5 +89,39 @@ public class Main {
             default:
                 break;
         }
+    }
+
+    public static String checkWinner() {
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(3, 5, 7);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for (List l : winning) {
+            if (playerPositions.containsAll(l)) {
+                return "Congratulations you won!";
+            } else if (cpuPositions.containsAll(l)) {
+                return "CPU wins! Sorry :(";
+            } else if (playerPositions.size() + cpuPositions.size() == 9) {
+                return "It's a CAT!";
+            }
+        }
+
+        return "";
     }
 }

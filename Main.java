948eda3ddeb98fea125c2d35d55f87ee0fba74d2
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-04-11 20:17:51 +0500

    created gameBoard for tictactoe

--- Main.java
+++ Main.java
@@ -1,2 +1,24 @@
 public class Main {
+    public static void main(String[] args) {
+
+        char[][] board = {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+
+        printBoard(board);
+
+    }
+
+    public static void printBoard(char[][] board) {
+        for (char[] row : board) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
 }
